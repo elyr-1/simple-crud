c8aa0619830f8f0351a6ceea7ece90f2f0c9df0a
elyrick <elyrick.manlagnit@gmail.com>
2022-08-10 13:11:14 +0800

    Angular simple-crud

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "user",
+    loadChildren: () => import("./user/user.module").then(m => m.UserModule)
+  },
+  {
+    path: "book",
+    loadChildren: () => import("./book/book.module").then(m => m.BookModule)
+  },
+  {
+    path: "blog",
+    loadChildren: () => import("./blog/blog.module").then(m => m.BlogModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
